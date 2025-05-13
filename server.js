6810753af4ea8e483ca27f5f0a544ab503e8c901
MajestySkie <buarandimas877@gmail.com>
2025-05-13 23:14:40 +0700

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ server.all('/', (req, res) => {
 });
 
 function keepAlive() {
-  server.listen(3000, () => {
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
     console.log("Server is Ready!! " + Date.now());
   });
 }

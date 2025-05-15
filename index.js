5b0e286ef571769a3054d0c4af4923c8824745f7
MajestySkie <buarandimas877@gmail.com>
2025-05-15 10:15:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const keepAlive = require('./server.js');
 keepAlive();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
 
 const client = new Client({
   intents: [
@@ -10,6 +12,14 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
   console.log(`Bot aktif sebagai ${client.user.tag}`);
@@ -22,6 +32,23 @@ client.on('messageCreate', (message) => {
 
   if (content.includes("tag") || content.includes("tags")) {
     message.reply("you can find it here (https://nelly.tools/tags)");
+  }
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'Terjadi error saat menjalankan command!',
+      ephemeral: true
+    });
   }
 });
 
